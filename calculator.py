95c8cb1b516a0b125e22aed5ee6ba986ef8657bc
thajwilliamlee <60524534+thajwilliamlee@users.noreply.github.com>
2020-02-14 15:19:15 -0700

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,1 +1,42 @@
+#print(int("four"))
+print("Welcome to our calculator")
+print()
+problem = input("What is your math problem? ")
+results = ""
+numbers= []
+operators= []
+for i in problem:
+    if i.isdigit():
+        numbers.append(i)
+    else:
+        operators.append(i)
 
+for i in operators:
+    if i == "*" or i == "/":
+        temp = operators.index(i)
+        if i == "*":
+            results = int(numbers[temp]) * int(numbers[temp + 1])
+        elif i == "/":
+            results = int(numbers[temp]) / int(numbers[temp + 1])
+        numbers.remove(numbers[temp])
+        numbers.remove(numbers[temp])
+        numbers.insert(temp,results)
+
+    elif i == "+" or i == "-":
+        if i == "+":
+            results = int(numbers[0]) + int(numbers[1])
+        elif i == "-":
+            results = int(numbers[0]) - int(numbers[1])
+        numbers.remove(numbers[0])
+        numbers.remove(numbers[0])
+        numbers.insert(0,results)
+
+
+
+print(numbers)
+print(operators)
+
+
+
+print(problem)
+print(results)
